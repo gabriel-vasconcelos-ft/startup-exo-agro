b9408693a95c65ce7d08fd0a7dbaac88db326a77
RubensLFerreira <rubenslima000@gmail.com>
2023-12-14 19:35:16 -0300

    Adicionando botão cadastrar clientes

--- src/pages/ListaCliente/index.jsx
+++ src/pages/ListaCliente/index.jsx
@@ -5,7 +5,15 @@ import Header2 from "../../components/Header2";
 import ButtonList from "../../components/ButtonList";
 import { AntDesign } from "@expo/vector-icons";
 
+import { useNavigation } from "@react-navigation/core";
+
 export default function ListaCliente() {
+  const navigation = useNavigation();
+
+  const botaoAddCliente = () => {
+    navigation.navigate("CadastroCliente");
+  };
+
   return (
     <View style={styles.container}>
       <Header2 />
@@ -14,7 +22,10 @@ export default function ListaCliente() {
       </View>
       <View style={styles.listContainer}>
         <Text style={styles.list}>Lista de clientes</Text>
-        <TouchableOpacity style={styles.buttonAdd}>
+        <TouchableOpacity
+          style={styles.buttonAdd}
+          onPress={() => botaoAddCliente()}
+        >
           <AntDesign name="plus" style={styles.buttonIcon} />
         </TouchableOpacity>
       </View>
