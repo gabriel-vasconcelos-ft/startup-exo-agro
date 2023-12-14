96b85ee02ed600bc746d1e320f4c9302d56b5472
RubensLFerreira <rubenslima000@gmail.com>
2023-12-14 19:36:10 -0300

    feat: serviço de cliente

--- src/service/ClienteService.js
+++ src/service/ClienteService.js
@@ -12,3 +12,19 @@ export const todosClientes = async () => {
     };
   }
 };
+
+export const criarCliente = async (clienteData) => {
+  try {
+    const cliente = await api.post(
+      "/cliente/cadastrar/tipo/cliente",
+      clienteData
+    );
+    return cliente.data;
+  } catch (error) {
+    console.error(error.message);
+    return {
+      code: 500,
+      message: "Erro ao criar cliente",
+    };
+  }
+};
